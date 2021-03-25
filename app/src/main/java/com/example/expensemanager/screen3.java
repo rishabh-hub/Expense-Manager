b1ae49b7e3505320cc3711ee99af352ec5e1a4b8
anub-dell <f20201161@goa.bits-pilani.ac.in>
2021-03-25 23:36:12 +0530

    10

--- app/src/main/java/com/example/expensemanager/screen3.java
+++ app/src/main/java/com/example/expensemanager/screen3.java
@@ -3,12 +3,82 @@ package com.example.expensemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.expensemanager.R;
+
+import java.util.ArrayList;
 
 public class screen3 extends AppCompatActivity {
+
+    TextView shopping;
+    TextView entertainment;
+    TextView travelling;
+    TextView food;
+    TextView household;
+    TextView others;
+    double shopping_sum;
+    double entertainment_sum;
+    double travelling_sum;
+    double food_sum;
+    double household_sum;
+    double others_sum;
+
+
+
+    ArrayList<String> category;
+    ArrayList<Integer> expense;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_screen3);
-    }
-}
+        setContentView(R.layout.activity_main);
+
+        shopping.findViewById(R.id.shopping);
+        entertainment.findViewById(R.id.entertainment);
+        travelling.findViewById(R.id.travel);
+        food.findViewById(R.id.food);
+        household.findViewById(R.id.household);
+        others.findViewById(R.id.others);
+
+        category=new ArrayList<>();
+        expense=new ArrayList<>();
+        category = getIntent().getStringArrayListExtra("");
+        expense = getIntent().getIntegerArrayListExtra("");
+
+        for (int i = 0; i < category.size(); i++)
+
+        {
+            if (category.get(i) == "shopping") {
+                shopping_sum = shopping_sum + expense.get(i);
+            } else if (category.get(i) == "leisure") {
+                entertainment_sum = entertainment_sum + expense.get(i);
+
+            } else if (category.get(i) == "travelling") {
+                travelling_sum = travelling_sum + expense.get(i);
+
+            } else if (category.get(i) == "food") {
+                food_sum = food_sum + expense.get(i);
+
+            } else if (category.get(i) == "household") {
+                household_sum = household_sum + expense.get(i);
+
+            } else {
+                others_sum = others_sum + expense.get(i);
+
+            }
+
+        }
+
+
+
+        shopping.setText(Double.toString(shopping_sum));
+        entertainment.setText(Double.toString(entertainment_sum));
+        travelling.setText(Double.toString(travelling_sum));
+        food.setText(Double.toString(food_sum));
+        household.setText(Double.toString(household_sum));
+        others.setText(Double.toString(others_sum));}}
